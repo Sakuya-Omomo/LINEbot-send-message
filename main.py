be8da6f5bfbbb2ad8bf5481c5630cec2560976d3
Sakuya-Omomo <bluewalker.39ya@gmail.com>
2026-09-09 19:22:28 +0900

    2回目ファイルの修正

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+
+load_dotenv()
+
+CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
+USER_ID = os.getenv("USER_ID")
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+
+
+def main():
+    message = TextSendMessage(text="テスト2週間前だよ\nそろそろ勉強しよう!!")
+
+    line_bot_api.push_message(USER_ID,messages=message)
+
+if __name__ == "__main__":
+    main()
